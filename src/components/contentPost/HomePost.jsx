796c34a8c3897702377bb7fd57833f9946339da3
rithish <vijaykosukumar@gmail.com>
2025-09-02 13:25:54 +0530

    main body post containor in progress 5

--- src/components/contentPost/HomePost.jsx
+++ src/components/contentPost/HomePost.jsx
@@ -1,23 +1,27 @@
 import ReplyTypePost from './postMiniComponents/ReplyTypePost'
 import IconPostWrite from './postMiniComponents/IconPostWrite'
+import PostCardMedia from './postMiniComponents/PostCardMedia'
 
 export default function HomePost({ }) {
     return (
-        <div className="w-full h-fit px-5 flex flex-row items-start border-solid border-b-[1px] border-gray-300/40">
-            <div className='flex flex-row w-fit py-2 mr-3'>
-                <div className="w-12 flex flex-col justify-start aspect-square rounded-full overflow-hidden">
-                    <img className="w-full h-full rounded-full object-cover" src="https://img.freepik.com/free-photo/closeup-view-calm-young-lady-street_74855-4995.jpg" alt="" />
+        <div className='w-full'>
+            <div className="w-full h-fit px-5 flex flex-row items-start border-solid border-b-[1px] border-gray-300/40">
+                <div className='flex flex-row w-fit py-2 mr-3'>
+                    <div className="w-12 flex flex-col justify-start aspect-square rounded-full overflow-hidden">
+                        <img className="w-full h-full rounded-full object-cover" src="https://img.freepik.com/free-photo/closeup-view-calm-young-lady-street_74855-4995.jpg" alt="" />
+                    </div>
+                </div>
+                <div className='w-full flex flex-col justify-center items-start pt-5'>
+                    <div className='w-full flex flex-col justify-center items-start mb-5'>
+                        <span className='text-xl px-1 text-gray-500'>What's happening?</span>
+                    </div>
+                    <div className='w-full'>
+                        <ReplyTypePost></ReplyTypePost>
+                        <IconPostWrite></IconPostWrite>
+                    </div>
                 </div>
             </div>
-            <div className='w-full flex flex-col justify-center items-start pt-5'>
-                <div className='w-full flex flex-col justify-center items-start mb-5'>
-                    <span className='text-xl px-1 text-gray-500'>What's happening?</span>
-                </div>
-                <div className='w-full'>
-                    <ReplyTypePost></ReplyTypePost>
-                    <IconPostWrite></IconPostWrite>
-                </div>
-            </div>
+            <PostCardMedia></PostCardMedia>
         </div>
     )
 };
