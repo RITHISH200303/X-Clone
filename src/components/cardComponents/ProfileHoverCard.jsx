8d928705d7f9cef16275e313bb6f45a603a6fd2a
vj <vijaykosukumar@gmail.com>
2026-01-06 10:32:39 -0500

    feat(profile-hover): add new Profile hover card component for user profile intraction

--- src/components/cardComponents/ProfileHoverCard.jsx
+++ src/components/cardComponents/ProfileHoverCard.jsx
@@ -1,8 +1,29 @@
 import { forwardRef } from "react"
+import FollowButton from "../buttons/FollowButton"
 
 export default forwardRef(function ProfileHoverCard({onMouseEnter, onMouseLeave},ref){
         return(
-            <dialog ref={ref} className="absolute top-9 left-0 p-4 rounded text-white outline-none" onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
+            <dialog ref={ref} className="bg-light-50 w-80 absolute top-9 left-0 p-4 rounded outline-none rounded-2xl" onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
+                <div className="flex flex-row justify-between w-full">
+                    <div className="w-14 flex flex-col justify-start aspect-square rounded-full overflow-hidden">
+                        <img className="w-full h-full object-cover" src="https://i.pinimg.com/736x/76/5b/bc/765bbc893a035f5517d151a411f5df72.jpg" alt=""/>
+                    </div>
+                    <div className="w-fit text-black text-[1rem] font-bold">
+                        <FollowButton/>
+                    </div>
+                </div>
+                <div>
+                    a
+                </div>
+                <div>
+                    b
+                </div>
+                <div>
+                    c
+                </div>
+                <div>
+                    d
+                </div>
                 
             </dialog>
         )
