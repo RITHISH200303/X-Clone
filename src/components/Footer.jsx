b24a460737d81cb12a96e1ba14492287adcdeaab
vj <vijaykosukumar@gmail.com>
2026-01-12 10:41:27 -0500

    feat(latest-news):added new component for latest news

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import Subscription from "./extras/Subscription"
 import SearchBar from "./extras/SearchBar"
+import TodaysNews from "./extras/TodaysNews"
 
 export default function Footer({ className }) {
     return (
@@ -7,6 +8,7 @@ export default function Footer({ className }) {
             <div className="flex flex-col w-[90%] gap-5 text-left">
                 <SearchBar className="w-[95%]"/>
                 <Subscription className="w-90% px-5 py-4 flex flex-col gap-3 rounded-lg border-solid border-1 border-gray-300/40"/>
+                <TodaysNews className="w-90% px-5 py-4 flex flex-col gap-3 rounded-lg border-solid border-1 border-gray-300/40"/>
             </div>
         </div>
     )
---
+++ src/components/labels/ProfileRepeat.jsx
@@ -0,0 +1,8 @@
+export default function ProfileRepeat({className, setRepeatCount, setLink}){
+    return(
+    <div className={className}>
+        <img className="w-full h-full object-cover" src="https://i.pinimg.com/736x/76/5b/bc/765bbc893a035f5517d151a411f5df72.jpg" alt=""/>
+    </div>
+
+    )
+}
