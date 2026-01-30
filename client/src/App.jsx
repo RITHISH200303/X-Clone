559134c12fd92381621e9626ae8bc4c84efd60d6
vj <vijaykosukumar@gmail.com>
2026-01-30 08:09:23 -0500

    ci(home-api): integrated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,6 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/home" element={<Home getRoute={"/api/home"} />} />
-          {/* test commit */}
         </Routes>
       </BrowserRouter>
     </div>
