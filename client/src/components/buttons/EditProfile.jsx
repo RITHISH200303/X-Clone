2ab205858b49e276660a9b00d64a358c3511ba9f
vj <vijaykosukumar@gmail.com>
2026-02-20 13:46:13 -0500

    refactor(user-profile): added top arrow & search

---
+++ client/src/components/buttons/EditProfile.jsx
@@ -0,0 +1,7 @@
+export function EditProfile({className}){
+    return(
+        <div className={`${className} w-fit rounded-3xl border-gray-300 border-[1.5px] border-solid align-middle px-4 py-1.5 mx-4 cursor-pointer font-bold`}>
+            Edit profile
+        </div>
+    )
+}
--- client/src/components/pageSections/userProfileSection/UserProfile.jsx
+++ client/src/components/pageSections/userProfileSection/UserProfile.jsx
@@ -1,15 +1,28 @@
 import "./userProfile.css";
+import { EditProfile } from "../../buttons/EditProfile";
+import { ArrowLeft } from "lucide-react";
+import { Search } from "lucide-react";
 
 export default function UserProfile({ className }) {
   return (
     <div className={className}>
-      <div className="flex flex-row justify-between px-4 py-1 items-center">
-        <span className="w-[8%]">--</span>
-        <div className="w-[84%] flex flex-col text-left px-2">
-          <span>Kumbi Kacheri</span>
-          <span>70</span>
+      <div className="flex flex-row justify-between px-3 py-1 items-center">
+        <div className="w-[6%]">
+          <span className="w-fit flex items-center cursor-pointer py-1.5 px-2 rounded-full hover:bg-gray-200">
+            <ArrowLeft className="w-5"></ArrowLeft>
+          </span>
         </div>
-        <span className="w-[8%]">--</span>
+        <div className="flex flex-col text-left px-2 w-[80%]">
+          <span className="text-xl font-bold">Mahatma Gandhi</span>
+          <span className="text-[13px] text-gray-500 font-medium">
+            70 posts
+          </span>
+        </div>
+        <div>
+          <span className="w-fit flex items-center cursor-pointer py-1.5 px-2 font-semibold rounded-full hover:bg-gray-200">
+            <Search className="w-5"></Search>
+          </span>
+        </div>
       </div>
       <div className="cover_pic_parent_cont relative">
         <div className="bg-gray-400 cover_pic_cont">
@@ -26,7 +39,17 @@ export default function UserProfile({ className }) {
             alt=""
           />
         </div>
-        <div className=""></div>
+        <div className="w-full flex flex-row justify-end my-3">
+          <EditProfile></EditProfile>
+        </div>
+        <div className="pt-5 flex flex-col items-start mx-4">
+          <div>
+            <span className="text-xl font-extrabold">Mahatma Gandhi</span>
+            <span></span>
+          </div>
+          <div className="font-medium text-gray-500">@mahatmagandhi</div>
+          <div className="text-lg mt-3">comments</div>
+        </div>
       </div>
     </div>
   );
